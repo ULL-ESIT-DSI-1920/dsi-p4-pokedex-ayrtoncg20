f0518da8aff1c357c0d459be7d94eda27cbbb183
ayrtoncg20 <alu0100839739@ull.edu.es>
2020-08-10 11:52:19 +0100

    Ya se pone la imagen de delante si pones el mouse encima

--- src/js/index.js
+++ src/js/index.js
@@ -11,12 +11,26 @@ function crear_pokedex() {
         document.querySelector('#pokedex').style.display = 'none';
         document.querySelector('#contenido').style.visibility = 'visible';
         obtenerPokemon()
-        mostrar_pokemons();
     })
 
 
 }
 
+const obtenerPokemon = async () => {
+
+    for (var numero_pokemon = 1; numero_pokemon <= 151; numero_pokemon++) {
+        await fetch("https://pokeapi.co/api/v2/pokemon/" + numero_pokemon + "/")
+            .then(data => data.json())
+            .then(data => {
+                const datos_pokemon = data;
+                const pokemon = new Pokemon(datos_pokemon)
+                insertar_pokemon(pokemon)
+            })
+
+    }
+    await mostrar_pokemons()
+
+}
 
 function insertar_pokemon(pokemon) {
     array_pokemons.push(pokemon);
@@ -24,33 +38,16 @@ function insertar_pokemon(pokemon) {
 
 }
 
-
-const obtenerPokemon = async () => {
-
-    for (var numero_pokemon = 1; numero_pokemon <= 151; numero_pokemon++) {
-        fetch("https://pokeapi.co/api/v2/pokemon/" + numero_pokemon + "/")
-            .then(data => data.json())
-            .then(data => {
-                const datos_pokemon = data;
-                const pokemon = new Pokemon(datos_pokemon)
-                insertar_pokemon(pokemon)
-            })
-    }
-
-
-}
-
-
-function mostrar_pokemons() {
+const mostrar_pokemons = async () => {
 
     const pokedex = document.getElementById("contenido");
 
     for (var i = 0; i < array_pokemons.length; i++) {
         pokedex.innerHTML += `
-                    <div class="fondo" style="background-color: grey; display: inline-block; align-items: center">
-                    ${array_pokemons[i].nombre} ${array_pokemons[i].id}
-                    <img src="${array_pokemons[i].imagen_detras}" class="detras">
-                    <img src="${array_pokemons[i].imagen_delante}" class="delante" width=\"150px\" height=\"150px\">
+                    <div class="carta">
+                        ${array_pokemons[i].id} ${array_pokemons[i].nombre} 
+                        <img src="${array_pokemons[i].imagen_detras}">
+                        <img src="${array_pokemons[i].imagen_delante}" class="delante">
                     </div>
                     `
     }
@@ -58,5 +55,3 @@ function mostrar_pokemons() {
 }
 
 crear_pokedex()
-
-

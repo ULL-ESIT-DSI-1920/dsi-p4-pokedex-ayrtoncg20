3e71fa16e639677659df61bd94fdbdff155a7832
ayrtoncg20 <alu0100839739@ull.edu.es>
2020-08-30 18:25:56 +0100

    terminando la practica

--- src/js/Pokemon.js
+++ src/js/Pokemon.js
@@ -1,13 +1,12 @@
 
-//Creamos la clase Pokemon, en esta clase tendremos el indentificador del pokemon, el nombre, así como su imagen delantera y trasera
+// Creamos la clase Pokemon, en esta clase tendremos el indentificador del pokemon, el nombre, así como su imagen delantera y trasera
 
 export default class Pokemon {
-    constructor(datos_pokemon) {
-        //Le pasamos por el constructor todos los datos del pokemon, de uno en particular
-        this.nombre = datos_pokemon.name; //El nombre de este pokemon será el nombre del objeto que le pasemos por parametro en este caso dentro de datos_pokemon buscamos la variable name
-        this.id = datos_pokemon.id; //Se busca dentro del objeto la variable id 
-        this.imagen_detras = datos_pokemon.sprites.back_default; //Guardamos la imagen de detras
-        this.imagen_delante = datos_pokemon.sprites.front_default; //guardamos la imagen de delante 
-    }
-
+  constructor (datos_pokemon) {
+    // Le pasamos por el constructor todos los datos del pokemon, de uno en particular
+    this.nombre = datos_pokemon.name // El nombre de este pokemon será el nombre del objeto que le pasemos por parametro en este caso dentro de datos_pokemon buscamos la variable name
+    this.id = datos_pokemon.id // Se busca dentro del objeto la variable id
+    this.imagen_detras = datos_pokemon.sprites.back_default // Guardamos la imagen de detras
+    this.imagen_delante = datos_pokemon.sprites.front_default // guardamos la imagen de delante
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,57 +1,46 @@
-import "regenerator-runtime/runtime"; //declaramos runtime para las promesas
-import Pokemon from './Pokemon.js'; //Importamos la clase pokemon que es otro fichero
+import 'regenerator-runtime/runtime' // declaramos runtime para las promesas
+import Pokemon from './Pokemon.js' // Importamos la clase pokemon que es otro fichero
 
-var array_pokemons = []; //declaramos un array de pokemon, cuando llamemos a cada pokemon lo vamos almacenando en este array 
+var array_pokemons = [] // declaramos un array de pokemon, cuando llamemos a cada pokemon lo vamos almacenando en este array
 
-
-function crear_pokedex() { //Cremos una funcion crear pokedex que se encargara de cuando toquemos el boton quitar la pokedex y mostrar los pokemon
-
-    const button = document.querySelector("#button"); //Seleccionamos el elemento boton
-    button.addEventListener("click", () => { //Si hay un evento de click
-        document.querySelector('#pokedex').style.display = 'none'; //La pokedex ponla a nula
-        document.querySelector('#contenido').style.visibility = 'visible'; //Pon el contenido a visible
-        obtenerPokemon() //Llamamos a la función obtener pokemon
-    })
-
-
+function crear_pokedex () { // Cremos una funcion crear pokedex que se encargara de cuando toquemos el boton quitar la pokedex y mostrar los pokemon
+  const button = document.querySelector('#button') // Seleccionamos el elemento boton
+  button.addEventListener('click', () => { // Si hay un evento de click
+    document.querySelector('#pokedex').style.display = 'none' // La pokedex ponla a nula
+    document.querySelector('#contenido').style.visibility = 'visible' // Pon el contenido a visible
+    obtenerPokemon() // Llamamos a la función obtener pokemon
+  })
 }
 
-const obtenerPokemon = async () => { //Tenemos que utilizar promesas por lo que declaramos un async 
-
-    for (var numero_pokemon = 1; numero_pokemon <= 151; numero_pokemon++) { //Recorremos el numero de pokemon que tenemos 
-        await fetch("https://pokeapi.co/api/v2/pokemon/" + numero_pokemon + "/") //Hacemos una promesa para que nos devuelva cada pokemon
-            .then(data => data.json()) //Guardamos en data el json de los datos del pokemon
-            .then(data => {
-                const datos_pokemon = data; //Guardamos en datos pokemon el json de los datos del pokemon guardado en data gracias a las promesas
-                const pokemon = new Pokemon(datos_pokemon) //Creamos un nuevo pokemon con los datos del mismo
-                insertar_pokemon(pokemon) //Llamamos a insertar pokemon que se encargara de insertar los pokemon en el array
-            })
-
-    }
-    await mostrar_pokemons() //Esperamos y cuando se haya realizado todo llamamos a la funcion mostrar pokemon
-
+const obtenerPokemon = async () => { // Tenemos que utilizar promesas por lo que declaramos un async
+  for (var numero_pokemon = 1; numero_pokemon <= 151; numero_pokemon++) { // Recorremos el numero de pokemon que tenemos
+    await fetch('https://pokeapi.co/api/v2/pokemon/' + numero_pokemon + '/') // Hacemos una promesa para que nos devuelva cada pokemon
+      .then(data => data.json()) // Guardamos en data el json de los datos del pokemon
+      .then(data => {
+        const datos_pokemon = data // Guardamos en datos pokemon el json de los datos del pokemon guardado en data gracias a las promesas
+        const pokemon = new Pokemon(datos_pokemon) // Creamos un nuevo pokemon con los datos del mismo
+        insertar_pokemon(pokemon) // Llamamos a insertar pokemon que se encargara de insertar los pokemon en el array
+      })
+  }
+  await mostrar_pokemons() // Esperamos y cuando se haya realizado todo llamamos a la funcion mostrar pokemon
 }
 
-function insertar_pokemon(pokemon) { //funcion insertar pokemon que se encargara de meter cada pokemon en el array creado al principio
-    array_pokemons.push(pokemon);
-
-
+function insertar_pokemon (pokemon) { // funcion insertar pokemon que se encargara de meter cada pokemon en el array creado al principio
+  array_pokemons.push(pokemon)
 }
 
-const mostrar_pokemons = async () => { //Tenemos la funcion mostrar pokemon 
+const mostrar_pokemons = async () => { // Tenemos la funcion mostrar pokemon
+  const pokedex = document.getElementById('contenido') // creamos una constante pokedex con el elemento contenido
 
-    const pokedex = document.getElementById("contenido"); //creamos una constante pokedex con el elemento contenido 
-
-    for (var i = 0; i < array_pokemons.length; i++) { //Recorremos todo el array de los pokemon, utilizamos innetHTML para meter un div carta con las imagenes y los datos del pokemon
-        pokedex.innerHTML += `
+  for (var i = 0; i < array_pokemons.length; i++) { // Recorremos todo el array de los pokemon, utilizamos innetHTML para meter un div carta con las imagenes y los datos del pokemon
+    pokedex.innerHTML += `
                     <div class="carta">
                         <img src="${array_pokemons[i].imagen_detras}">
                         <img src="${array_pokemons[i].imagen_delante}" class="delante"><br>
                         ${array_pokemons[i].id} ${array_pokemons[i].nombre} 
                     </div>
                     `
-    }
-
+  }
 }
 
-crear_pokedex() //Llamamos a crear pokedex para inicializar toda la practica 
+crear_pokedex() // Llamamos a crear pokedex para inicializar toda la practica
